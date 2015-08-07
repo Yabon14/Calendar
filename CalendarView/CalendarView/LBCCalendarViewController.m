ed8cad1ac1b06cd9c8feea73eea3d2601ba0da52
Yann BONAFONS <yann.bonafons@scmfrance.fr>
2015-08-07 17:12:54 +0200

    update test

--- CalendarView/CalendarView/LBCCalendarViewController.m
+++ CalendarView/CalendarView/LBCCalendarViewController.m
@@ -24,17 +24,30 @@
     
     UITapGestureRecognizer *tg = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(addSelection:)];
     [self.view addGestureRecognizer:tg];
+
+
     NSDateComponents *dateComponents = [[NSDateComponents alloc] init];
-    [dateComponents setDay:10];
+    [dateComponents setDay:24];
     NSDate *startDate = [[NSCalendar currentCalendar] dateByAddingComponents:dateComponents toDate:[NSDate date] options:0];
-    [dateComponents setDay:30];
+    [dateComponents setDay:31];
     NSDate *endDate = [[NSCalendar currentCalendar] dateByAddingComponents:dateComponents toDate:[NSDate date] options:0];
+    
+    [dateComponents setDay:30];
+    NSDate *startDate2 = [[NSCalendar currentCalendar] dateByAddingComponents:dateComponents toDate:[NSDate date] options:0];
+    [dateComponents setDay:40];
+    NSDate *endDate2 = [[NSCalendar currentCalendar] dateByAddingComponents:dateComponents toDate:[NSDate date] options:0];
+    
+    [dateComponents setDay:45];
+    NSDate *startDate3 = [[NSCalendar currentCalendar] dateByAddingComponents:dateComponents toDate:[NSDate date] options:0];
+    [dateComponents setDay:48];
+    NSDate *endDate3 = [[NSCalendar currentCalendar] dateByAddingComponents:dateComponents toDate:[NSDate date] options:0];
     
     
     LBCSelection *selec = [[LBCSelection alloc] initWithStartDate:startDate andEndDate:endDate];
+    LBCSelection *selec2 = [[LBCSelection alloc] initWithStartDate:startDate2 andEndDate:endDate2];
+    LBCSelection *selec3 = [[LBCSelection alloc] initWithStartDate:startDate3 andEndDate:endDate3];
     
-    [self.calendarObject addSelectionArray:@[selec]];
-    //    [self.calendarObject addSelectionFromDate:startDate toDate:endDate];
+    [self.calendarObject addSelectionArray:@[selec, selec2, selec3]];
 }
 
 
@@ -43,14 +56,11 @@
     NSDateComponents *dateComponents = [[NSDateComponents alloc] init];
     [dateComponents setDay:24];
     NSDate *startDate = [[NSCalendar currentCalendar] dateByAddingComponents:dateComponents toDate:[NSDate date] options:0];
-    [dateComponents setDay:30];
+    [dateComponents setDay:31];
     NSDate *endDate = [[NSCalendar currentCalendar] dateByAddingComponents:dateComponents toDate:[NSDate date] options:0];
     
-    
-    LBCSelection *selec = [[LBCSelection alloc] initWithStartDate:startDate andEndDate:endDate];
-    
-    [self.calendarObject addSelectionArray:@[selec]];
-    //    [self.calendarObject addSelectionFromDate:startDate toDate:endDate];
+   
+    [self.calendarObject addSelectionFromDate:startDate toDate:endDate];
 }
 
 @end
