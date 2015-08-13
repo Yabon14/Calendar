ec008976633c4c8f4f3b2faa3a4061f5eaabdd49
Yann BONAFONS <yann.bonafons@scmfrance.fr>
2015-08-13 15:42:46 +0200

    manage label font size

--- CalendarView/CalendarView/LBCCalendar/LBCCalendarHeaderView.m
+++ CalendarView/CalendarView/LBCCalendar/LBCCalendarHeaderView.m
@@ -16,6 +16,7 @@
 
     self.monthLabel.backgroundColor = [UIColor clearColor];
     self.monthLabel.text = [calendarObject getCurrentMonthYearName];
+    self.monthLabel.font = F12;
         
     self.leftButton.tag = LEFT_ARROW_TAG;
     self.rightButton.tag = RIGHT_ARROW_TAG;
@@ -29,43 +30,31 @@
     NSArray *dayArray = @[@"Sa", @"Di", @"Lu", @"Ma", @"Me", @"Je", @"Ve"];
     NSArray *dayLabelArray = @[self.dayLabel1, self.dayLabel2, self.dayLabel3, self.dayLabel4, self.dayLabel5, self.dayLabel6, self.dayLabel7];
 
-    CGFloat fontSize = 200;
     for (UILabel *dayLabel in dayLabelArray) {
         dayLabel.backgroundColor = [UIColor clearColor];
-//        dayLabel.numberOfLines = 1;
         dayLabel.textAlignment = NSTextAlignmentCenter;
-//        dayLabel.minimumScaleFactor = 0.5f;
-//        dayLabel.font = [UIFont systemFontOfSize:fontSize];
-        [dayLabel setAdjustsFontSizeToFitWidth:YES];
         dayLabel.text = [dayArray objectAtIndex:[dayLabelArray indexOfObject:dayLabel]];
-        [self setMaximumFontFrom:300 inLabel:dayLabel withMinimumScaleFactor:2.0f];
-        NSLog(@"dayLabel.font.pointSize: %lu", (long)dayLabel.font.pointSize);
-//        fontSize = fontSize > dayLabel.font.pointSize ? dayLabel.font.pointSize : fontSize;
+        dayLabel.font = F5;
     }
-//    for (UILabel *dayLabel in dayLabelArray) {
-//        dayLabel.font = [UIFont systemFontOfSize:fontSize];
-//    }
-    
-    self.backgroundColor = [UIColor clearColor];
 }
 
 
 
-- (void) setMaximumFontFrom:(CGFloat)fontSize inLabel:(UILabel *)label withMinimumScaleFactor:(CGFloat)scaleFactor{
-
-    CGRect frame = label.frame;
-    
-    CGRect newFrame;
-    do{
-        label.font = [UIFont systemFontOfSize:fontSize];
-        [label sizeToFit];
-        newFrame = label.frame;
-        fontSize--;
-    } while (newFrame.size.width > frame.size.width || newFrame.size.height > frame.size.height);
-    
-    fontSize *= scaleFactor;
-    label.font = [UIFont systemFontOfSize:fontSize];
-}
+//- (void) setMaximumFontFrom:(CGFloat)fontSize inLabel:(UILabel *)label withMinimumScaleFactor:(CGFloat)scaleFactor{
+//
+//    CGRect frame = label.frame;
+//    
+//    CGRect newFrame;
+//    do{
+//        label.font = [UIFont systemFontOfSize:fontSize];
+//        [label sizeToFit];
+//        newFrame = label.frame;
+//        fontSize--;
+//    } while (newFrame.size.width > frame.size.width || newFrame.size.height > frame.size.height);
+//    
+//    fontSize *= scaleFactor;
+//    label.font = [UIFont systemFontOfSize:fontSize];
+//}
 
 
 @end
